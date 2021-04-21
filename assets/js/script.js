712dc8c7680762bdce698eb34b0f4b9d2621ebab
Giuseppe Napoli <giuseppe.napoli1993@hotmail.it>
2021-04-21 19:20:59 +0200

    Terminato

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(function(){
+  // MENU A TENDINA //
+  /* creo degli eventi che facciano apparire/scomparire i menù */
+
+  /* al passaggio del mouse sul 'menu a tendina' si aggiungerà la classe active al 'td-menu' */
+  $('.menu-tendina').mouseenter(function(){
+    $(this).find('.td-menu').addClass('active')
+  })
+  /* al click del mouse sul 'menu a tendina' si rimuoverà la classe active al 'td-menu' */
+  $('.menu-tendina').click(function(){
+    $(this).find('.td-menu').removeClass('active')
+  })
+  /* all'uscita del mouse sul 'menu a tendina' si rimuoverà la classe active al 'td-menu' */
+  $('.menu-tendina').mouseleave(function(){
+    $(this).find('.td-menu').removeClass('active')
+  })
+
+});
